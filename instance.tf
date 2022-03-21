b27d5e7d327538298122b6d2ddb2453625845575
olluseun <ollu1.ajani@gmail.com>
2022-03-21 23:22:17 +0000

    update error

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ module "spin_up_machine" {
     source = "./mymodules"
     instance_count = 2
     node_name = ["apache_Web_Server","Tomcat_web_Server"]
+    environment = "dev"
 }
 
 module "spin2_up_machine" {
