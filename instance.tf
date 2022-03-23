fabb71c4c389c73e8fffe0c534a99da393dc8022
olluseun <ollu1.ajani@gmail.com>
2022-03-23 20:11:02 +0000

    new changes

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
-module "spin_up_machine" {
+module "machine" {
     source = "./mymodules"
     instance_count = 2
     node_name = ["apache_Web_Server","Tomcat_web_Server"]
     environment = "dev"
 }
 
-module "spin2_up_machine" {
+module "machine2" {
     source = "./mymodules"
     instance_count = 2
     node_name = ["apache_Web_Server","Tomcat_web_Server"]
