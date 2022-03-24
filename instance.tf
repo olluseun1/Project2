305038ae4981e271a7f4f69b185562d12c5fe5cf
olluseun <ollu1.ajani@gmail.com>
2022-03-24 22:21:46 +0000

    change instance to 1

--- instance.tf
+++ instance.tf
@@ -1,13 +1,13 @@
 module "machine" {
     source = "./mymodules"
-    instance_count = 2
+    instance_count = 1
     node_name = ["apache_Web_Server","Tomcat_web_Server"]
     environment = "dev"
 }
 
 module "machine2" {
     source = "./mymodules"
-    instance_count = 2
-    node_name = ["apache_Web_Server","Tomcat_web_Server"]
+    instance_count = 1
+    node_name = ["apache Web Server","Tomcat_web_Server"]
     environment = "dev"
 }
